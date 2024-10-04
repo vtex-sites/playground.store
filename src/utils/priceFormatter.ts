9d5f491828b95151b855da770f5b93556fae9c00
Fanny Chien <fannychienn@gmail.com>
2024-10-04 19:44:14 -0300

    chore: Updates faststore.config.js reference and renaming to discovery

--- src/utils/priceFormatter.ts
+++ src/utils/priceFormatter.ts
@@ -1,4 +1,4 @@
-import FaststoreConfig from "../../faststore.config.js";
+import FaststoreConfig from "../../discovery.config.js";
 
 export const priceFormatter = (value: number) => {
   return value.toLocaleString(FaststoreConfig.session.locale, {
