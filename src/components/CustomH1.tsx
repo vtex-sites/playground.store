6854f82323a73b8e4be621aeed55492d09095d0b
Larícia Mota <laricia.mota@vtex.com.br>
2025-01-30 12:26:58 -0300

    Update version

--- src/components/CustomH1.tsx
+++ src/components/CustomH1.tsx
@@ -1,15 +1,11 @@
-import { usePLP } from "@faststore/core";
+import { usePLP } from '@faststore/core'
 
-export interface CustomH1Props {
-  title: string;
-}
-
-export default function CustomH1(props: CustomH1Props) {
-  const context = usePLP();
+export default function CustomH1() {
+  const context = usePLP()
 
   return (
     <section>
-      <h1>Testando: {context.data?.collection?.seo?.title}!</h1>
+      <h1>Testing - {context.data?.collection?.seo?.title ?? 'PLP'}!</h1>
     </section>
-  );
+  )
 }
