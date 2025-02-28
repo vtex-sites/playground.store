07cbdcb2151026aaef5373cdf861233b4149aef0
eduardoformiga <eduardo.formiga@gmail.com>
2025-02-27 22:39:49 -0300

    feat: adds CustomNewsletterSection

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import CustomIconsAlert from "./sections/CustomIconsAlert/CustomIconsAlert";
 import AlertWithImage from "./sections/AlertWithImage/AlertWithImage";
 import CustomProductDetails from "./sections/CustomProductDetails/CustomProductDetails";
+import CustomNewsletter from "./sections/CustomNewsletter/CustomNewsletter";
 import ContactForm from "./ContactForm/ContactForm";
 
 const sections = {
@@ -8,6 +9,7 @@ const sections = {
   AlertWithImage,
   ProductDetails: CustomProductDetails,
   ContactForm,
+  CustomNewsletter,
 };
 
 export default sections;
