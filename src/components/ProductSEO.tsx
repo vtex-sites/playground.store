2877bafd3c5af72744c9c0b119c619ca7ff24507
fanny <fanny.chien@vtex.com>
2025-09-30 21:01:31 -0300

    chore: Adds product SEO to override default ProductJsonLd

---
+++ src/components/ProductSEO.tsx
@@ -0,0 +1,22 @@
+import { usePDP } from "@faststore/core";
+import { ProductJsonLd } from "next-seo";
+
+export function ProductSEO() {
+  // FastStore exposes the data that comes from FastStore API along with FastStore API Extensions inside a provider. Use the usePDP hook to access data from a Product Detail Page (PDP). Refer to: https://developers.vtex.com/docs/guides/faststore/api-extensions-consuming-api-extensions
+  const context = usePDP();
+  const product = context?.data?.product;
+  const meta = product?.seo;
+
+  return (
+    <ProductJsonLd
+      productName={meta.title}
+      description={meta.description}
+      brand={product.brand.name}
+      sku="TESTSKU"
+      gtin={product.gtin}
+      releaseDate={product.releaseDate}
+    />
+  );
+}
+
+export default ProductSEO;
--- src/components/sections/CustomProductDetails/CustomProductDetails.tsx
+++ src/components/sections/CustomProductDetails/CustomProductDetails.tsx
@@ -1,10 +1,12 @@
 import { getOverriddenSection, ProductDetailsSection } from "@faststore/core";
 import { BuyButtonWithDetails } from "../../BuyButtonWithDetails/BuyButtonWithDetails";
+import { ProductSEO } from "../../ProductSEO";
 
 const CustomProductDetails = getOverriddenSection({
   Section: ProductDetailsSection,
   components: {
     BuyButton: { Component: BuyButtonWithDetails },
+    __experimentalShippingSimulation: { Component: ProductSEO },
   },
 });
 
