2a4f3d12685011bd05ee5358104f3505a969fc50
fanny <fanny.chien@vtex.com>
2025-10-03 13:21:13 -0300

    chore: creates custom product seo section

--- src/components/sections/CustomProductDetails/CustomProductDetails.tsx
+++ src/components/sections/CustomProductDetails/CustomProductDetails.tsx
@@ -1,12 +1,10 @@
 import { getOverriddenSection, ProductDetailsSection } from "@faststore/core";
 import { BuyButtonWithDetails } from "../../BuyButtonWithDetails/BuyButtonWithDetails";
-import { CustomProductSEO } from "../../ProductSEO";
 
 const CustomProductDetails = getOverriddenSection({
   Section: ProductDetailsSection,
   components: {
     BuyButton: { Component: BuyButtonWithDetails },
-    __experimentalShippingSimulation: { Component: CustomProductSEO },
   },
 });
 
--- src/components/sections/CustomProductSEO/CustomProductSEO.tsx
+++ src/components/sections/CustomProductSEO/CustomProductSEO.tsx
@@ -1,22 +1,24 @@
 import { usePDP } from "@faststore/core";
 import { ProductJsonLd } from "next-seo";
 
-export function CustomProductSEO() {
+export function CustomProductSEOSection() {
   // FastStore exposes the data that comes from FastStore API along with FastStore API Extensions inside a provider. Use the usePDP hook to access data from a Product Detail Page (PDP). Refer to: https://developers.vtex.com/docs/guides/faststore/api-extensions-consuming-api-extensions
   const context = usePDP();
   const product = context?.data?.product;
   const meta = product?.seo;
 
   return (
-    <ProductJsonLd
-      productName={meta.title}
-      description={meta.description}
-      brand={product.brand.name}
-      sku="TESTSKU"
-      gtin={product.gtin}
-      releaseDate={product.releaseDate}
-    />
+    <section>
+      <ProductJsonLd
+        productName={meta.title}
+        description={meta.description}
+        brand={product.brand.name}
+        sku="TESTSKU"
+        gtin={product.gtin}
+        releaseDate={product.releaseDate}
+      />
+    </section>
   );
 }
 
-export default CustomProductSEO;
+export default CustomProductSEOSection;
