322fbd53b299fb83c1a9072b38910ff40277eaec
fanny <fanny.chien@vtex.com>
2025-10-03 22:37:40 -0300

    chore: test site verification

---
+++ src/scripts/ThirdPartyScripts.tsx
@@ -0,0 +1,10 @@
+const ThirdPartyScripts = () => {
+  return (
+    <meta
+      name="google-site-verification"
+      content="xRwnzq5B91_3hXsAxoKXfxRwMWk2wsaNwInIjiibTx0"
+    />
+  );
+};
+
+export default ThirdPartyScripts;
